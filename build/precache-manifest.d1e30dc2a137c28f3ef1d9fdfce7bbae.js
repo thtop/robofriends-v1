3b501ab92a059d6529aa8531555044c1745d6b14
Thamonwan Maneechan <47081950+thtop@users.noreply.github.com>
2019-04-14 11:30:18 +0700

    React RoboFriends V1

---
+++ build/precache-manifest.d1e30dc2a137c28f3ef1d9fdfce7bbae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c98141ea740753ce0126",
+    "url": "/static/css/main.e352034c.chunk.css"
+  },
+  {
+    "revision": "c98141ea740753ce0126",
+    "url": "/static/js/main.00f81490.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c5f72922c9c73fcba05e",
+    "url": "/static/css/2.294af5e7.chunk.css"
+  },
+  {
+    "revision": "c5f72922c9c73fcba05e",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d7b403e9c26ac59030193c739f4e3526",
+    "url": "/index.html"
+  }
+];
